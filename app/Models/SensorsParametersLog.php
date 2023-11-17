ec4a8f90c37e4e480888347958109294eece8f21
agungdn <luckymen616.ad@gmail.com>
2023-11-17 10:22:41 +0700

    initial commit

--- app/Models/SensorsParametersLog.php
+++ app/Models/SensorsParametersLog.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Laililmahfud\Adminportal\Traits\HasUuid;
 
-class SensorsParametersLog extends Model implements
+class SensorsParametersLog extends Model
 {
     use HasFactory, HasUuid;
 }
